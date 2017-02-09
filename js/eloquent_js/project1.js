d9e27b7cd1d268d1ff83902024a7ab85a0eec175
essviv <514912821@qq.com>
2017-02-09 09:28:33 +0800

    add eloquent chapter 8 - regular expression

--- js/eloquent_js/project1.js
+++ js/eloquent_js/project1.js
@@ -2,6 +2,8 @@
  * Created by sunyiwei on 2017/2/8.
  */
 
+"use strict";
+
 function Vector(x, y) {
     this.x = x;
     this.y = y;
@@ -360,3 +362,174 @@ for (var i = 0; i < 10; i++) {
     world.turn();
     console.log(world.toString());
 }
+
+
+//test vector.plus
+function testVectorPlus() {
+    var p1 = new Vector(10, 20);
+    var p2 = new Vector(23, 35);
+    var p3 = p1.plus(p2);
+
+    if (p3.x != 33) {
+        return "error: x from plus";
+    }
+    if (p3.y != 55) {
+        return "error: y from plus";
+    }
+    if (p1.x != 10) {
+        return "error: x property";
+    }
+    if (p1.y != 20) {
+        return "error: y property";
+    }
+    if (p2.x != 23) {
+        return "error: x property";
+    }
+    if (p2.y != 35) {
+        return "error: y property";
+    }
+
+    return "everything is ok.";
+}
+
+console.log(testVectorPlus());
+
+//测试正则表达式
+var reg1 = new RegExp("abc");
+console.log(reg1.test("fdsabcde"));
+console.log(reg1.test("fdsacde"));
+
+var reg2 = /[0123456789]/;
+console.log(reg2.test('hello world 2'));
+console.log(reg2.test('hello world'));
+
+var reg3 = /[a-z]/;
+console.log(reg3.test('34344h'));
+console.log(reg3.test('34344'));
+
+var reg4 = /\d/;
+console.log(reg4.test('fdksfd'));
+console.log(reg4.test('fdksfdf3'));
+
+var reg5 = /\s/;
+console.log(reg5.test("fdsafdsa fdjskj"));
+console.log(reg5.test("fdsafdsa     fdjskj"));
+console.log(reg5.test("fdsafdsa_fdjskj"));
+
+var reg6 = /\S/;
+console.log(reg6.test("     "));
+console.log(reg6.test("  i  "));
+
+var reg7 = /./;
+console.log(reg7.test("\n\n\n\n"));
+console.log(reg7.test("\nfd"));
+
+var reg8 = /\D/;
+console.log(reg8.test("343434"));
+console.log(reg8.test("343434d"));
+
+var dateReg = /\d\d\d\d-\d\d-\d\d \d\d:\d\d/;
+console.log(dateReg.test("2017-02-03 15:00"));
+console.log(dateReg.test("2017-02-03T15:00"));
+
+var reg9 = /[\d.+?]/;
+console.log(reg9.test("43433."));
+console.log(reg9.test("43433?"));
+console.log(reg9.test("43433+"));
+console.log(reg9.test("DSDDSx"));
+
+var reg10 = /neighbou?r/;
+console.log(reg10.test('neighbour'));
+console.log(reg10.test('neighbor'));
+
+var reg11 = /\d*/;
+console.log(reg11.test("fdsafdsad"));
+console.log(reg11.test("fdsafd34342i"));
+
+var reg12 = /abc\d+/;
+console.log(reg12.test("343abc343"));
+console.log(reg12.test("343abc"));
+
+var reg13 = /1[3578]\d{9}/; //mobile
+console.log(reg13.test("18432421234"));
+console.log(reg13.test("1843241234"));
+console.log(reg13.test("14843241234"));
+
+var improvedDate = /\d{4}-\d{1,2}-\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}/;
+console.log(improvedDate.test("2017-12-01 1:2:3"));
+console.log(improvedDate.test("2017-12-01 12:2:3"));
+console.log(improvedDate.test("2017-12-1 12:2:3"));
+console.log(improvedDate.test("2017-12-1 12:2:3"));
+
+var reg15 = /'([^']*)'/;
+console.log(reg15.exec("I said 'hello'"));
+
+var reg16 = /(\d)+/;
+console.log(reg16.exec('one two 100 234'));
+
+function findDate(str) {
+    var dateReg = /^(\d{4})-(\d{1,2})-(\d{1,2}) (\d{1,2}):(\d{1,2}):(\d{1,2})$/;
+    var match = dateReg.exec(str);
+    if (match) {
+        return new Date(Number(match[1]),
+            Number(match[2]) - 1,
+            Number(match[3]),
+            Number(match[4]),
+            Number(match[5]),
+            Number(match[6]));
+    }
+}
+
+console.log(findDate("2017-02-03 13:29:39"));
+console.log(findDate("s2017-02-03 13:29:39"));
+
+var reg17 = /cat/;
+console.log(reg17.test("concatenate"));
+console.log(reg17.test("dsdf cat dfs"));
+
+var reg18 = /\bcat\b/;
+console.log(reg18.test("concatenate"));
+console.log(reg18.test("dsdf cat dfs"));
+
+var reg19 = /\b\d+\s(pig|cow|duck)s?\b/;
+console.log(reg19.test('15pigs'));
+console.log(reg19.test('15 pigs'));
+console.log(reg19.test('15 cows'));
+console.log(reg19.test('15 ducks'));
+console.log(reg19.test('1 duck'));
+console.log(reg19.test('duck'));
+
+var reg20 = /\b([01]+b|\d+|[\da-f]+h)\b/;
+console.log(reg20.test("3434343"));
+console.log(reg20.test("0101010100111b"));
+console.log(reg20.test("24323141413abe2343fh"));
+console.log(reg20.test("24323141413abe2343gh"));
+
+var reg21 = /g/g;
+var reg22 = /g/;
+var ori = 'google';
+console.log(ori.replace(reg21, 'k'));
+console.log(ori.replace(reg22, 'k'));
+
+var testStr = '1  apple, 13 lemons, 5 watermelons.';
+var reg24 = /(\d+)\s+(\w+)/g;
+console.log(testStr.replace(reg24, function(match, count, unit){
+    var newCount = Number(count) - 1;
+    if(newCount == 0){
+        newCount = 'no';
+    }
+
+    if(newCount == 1){
+        unit = unit.slice(0, unit.length - 1);
+    }
+
+    return newCount + " " + unit;
+}));
+
+var stripComment = /\/\/.*|\/\*[^]*\*\//g;
+console.log('var x = 10; //assign x'.replace(stripComment, ""));
+console.log('var x /* nonsense comment */ = 10;'.replace(stripComment, ""));
+
+var stripCommentNoGreedy = /\/\/.*|\/\*[^]*?\*\//g;
+console.log(' 1 /* nonsense */ + /* nonsense */ 1'.replace(stripComment, "") );
+console.log(' 1 /* nonsense */ + /* nonsense */ 1'.replace(stripCommentNoGreedy, "") );
